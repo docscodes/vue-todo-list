e31425a73ef72e5ae3707bda5677902eb668c423
docscodes <docscodes@gmail.com>
2024-11-02 16:20:04 -0400

    Sorting the list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import type { Ref } from 'vue'
 import ListItem from './ListItem.vue'
 
@@ -31,6 +31,10 @@ const findItemInList = (item: Item): Item | undefined => {
 const toggleItemChecked = (item: Item): void => {
   item.checked = !item.checked
 }
+
+const sortedList = computed(() =>
+  [...listItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
+)
 </script>
 
 <template>
