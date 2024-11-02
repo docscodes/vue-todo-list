901b101442318f7f44a4b719fd72b04fa0a17f81
docscodes <docscodes@gmail.com>
2024-11-02 16:16:30 -0400

    Making a list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,33 @@
 <script lang="ts" setup>
 import ListItem from './ListItem.vue'
+
+type Item = {
+  title: string,
+  checked?: boolean
+}
+
+const listItems: Item[] = [
+  { title: 'Make a todo list app', checked: true },
+  { title: 'Predict the weather', checked: false },
+  { title: 'Play some tunes', checked: false },
+  { title: 'Let\'s get cooking', checked: false },
+]
 </script>
+
 <template>
-<ul>
-  <ListItem :is-checked="false">This is the slotted content</ListItem>
-</ul>
+  <ul>
+    <li :key='key' v-for='(item, key) in listItems'>
+      <ListItem :is-checked='item.checked'>{{ item.title }}</ListItem>
+    </li>
+  </ul>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+
+li {
+  margin: 0.4rem 0;
+}
+</style>
